6d0dbc2c7f694f3c69509dc5eaad2761538b30ce
Menrui <Menrui.d@gmail.com>
2019-11-26 14:21:07 +0900

    filtering update

--- kadai3/33filtering.cpp
+++ kadai3/33filtering.cpp
@@ -14,17 +14,9 @@ cv::Mat filtering(cv::Matx33d kernel, cv::Mat image)
 	for (int y = 0; y < rows; y++) {
 		for (int x = 0; x < cols; x++) {
 			center_value = 0;
+			
 			for (int n = -1; n <= 1; n++) {
 				for (int m = -1; m <= 1; m++) {
-//					if (x + m < 0 && y + n < 0) {
-//						center_value += image.at<Vec3b>(Point(0, 0)) * kernel_.at<double>(Point(m + 1, n + 1));
-//					}
-//					else if (x + m < 0) {
-//						center_value += image.at<Vec3b>(Point(0, y + n)) * kernel_.at<double>(Point(m+1, n+1));
-//					}
-//					else if (y + n < 0) {
-//						center_value += image.at<Vec3b>(Point(x + m, 0)) * kernel_.at<double>(Point(m+1, n+1));
-//					}
 					if (x + m < 0 || y + n < 0 || x + m >= cols || y + n >= rows) {
 						center_value += Vec3b::ones()*128 * kernel_.at<double>(Point(m+1, n+1));
 					}
@@ -34,14 +26,20 @@ cv::Mat filtering(cv::Matx33d kernel, cv::Mat image)
 					
 				}
 			}
-//			cout << kernel(1, 1) << endl;
-//			cout << image.at<Vec3b>(Point(x, y)) * kernel(1, 1) << endl;
-//			cout << center_value << endl;
-//			cout << image.at<Vec3b>(Point(x, y)) << endl;
+
 			image_.at<Vec3b>(Point(x, y)) = center_value;
 		}
 	}
 	return image_;
+}
+
+cv::Mat averagingFilter(cv::Mat image)
+{
+	Matx33d averaging_kernel;
+	averaging_kernel << 1.0/9.0, 1.0 /9.0, 1.0 /9.0, 1.0 /9.0, 1.0 /9.0, 1.0 /9.0, 1.0 /9.0, 1.0 /9.0, 1.0 /9.0;
+	cout << "averaging_kernel=\n" << averaging_kernel << endl;
+
+	return filtering(averaging_kernel, image);
 }
 
 cv::Mat gaussianFilter(double sigma, cv::Mat image)
@@ -56,7 +54,7 @@ cv::Mat gaussianFilter(double sigma, cv::Mat image)
 		}
 	}
 	gaussian_kernel = gaussian_kernel * (1.0 / sum);
-	cout << gaussian_kernel << endl;
+	cout << "gaussian_kernel=\n" << gaussian_kernel << endl;
 	Mat image_ = filtering(gaussian_kernel, image);
 
 	return image_;
@@ -65,5 +63,31 @@ cv::Mat gaussianFilter(double sigma, cv::Mat image)
 double gaussianFunction(double sigma, int x, int y) {
 	double value = (1.0 / (2.0 * M_PI * pow(sigma, 2))) * exp(-(pow(x, 2) + pow(y, 2)) / (2.0 * pow(sigma, 2)));
 	return value;
+}
+
+cv::Mat sobelFilter(cv::Mat image, sobelMode mode)
+{
+	Matx33d sobel_kernel;
+	switch (mode)
+	{
+	case sobelMode::vertical:
+		sobel_kernel << -1.0, 0.0, 1.0, -2.0, 0.0, 2.0, -1.0, 0.0, 1.0;
+		break;
+	case sobelMode::horizontal:
+		sobel_kernel << -1.0, -2.0, -1.0, 0, 0, 0, 1.0, 2.0, 1.0;
+		break;
+	}
+	cout << "sobel_kernel=\n" << sobel_kernel << endl;
+
+	return filtering(sobel_kernel, image);
+}
+
+cv::Mat laplacianFilter(cv::Mat image)
+{
+	Matx33d laplacian_kernel;
+	laplacian_kernel << 0.0, 1.0, 0.0, 1.0, -4.0, 1.0, 0.0, 1.0, 0.0;
+	cout << "laplacian_kernel=\n" << laplacian_kernel << endl;
+
+	return filtering(laplacian_kernel, image);
 }
 
--- kadai3/main.cpp
+++ kadai3/main.cpp
@@ -9,6 +9,10 @@ int main() {
 
 	imshow("Origin", image);
 	imshow("Gaussian", gaussian_image);
+	imshow("Averaging", averagingFilter(image));
+	imshow("Sobel_horizontal", sobelFilter(image, sobelMode::horizontal));
+	imshow("Sobel_vertical", sobelFilter(image, sobelMode::vertical));
+	imshow("Laplacian", laplacianFilter(image));
 	waitKey();
 
 	return 1;
